04eb26a13d227b27ef69c24b3c5028f3b638349a
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-01-24 14:22:53 -0800

    added announcemet, scroll, scroll, sperator, and page header components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Nav from "@/components/Nav";
 import { CalendarDemo } from "@/components/CalendarForm";
 import { CardWithForm } from "@/components/CardForm";
 import { CommandDemo } from "@/components/CommandForm";
@@ -7,35 +6,68 @@ import { TableDemo } from "@/components/TableForm";
 import { ProfileForm } from "@/components/UserForm";
 import { Button } from "@/components/ui/button";
 
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header";
+import { Announcement } from "@/components/announcement";
+import { Nav } from "@/components/Nav";
+
+
 export default function Home() {
   return (
-    <main className="p-24">
-      <Nav />
-      <section className="py-8 flex flex-col items-center text-center gap-4">
-        <h1 className="text-6xl font-bold">Shadcn is Amazing</h1>
-        <p className="text-2xl text-muted-foreground">
-          Hand-picked themes that you can copy and paste into your apps <br />
-        </p>
-      </section>
-      <div className="flex items-center justify-center gap-2">
-        <Button variant={"secondary"}>Learn More</Button>
-        <Button variant={"default"}>Enroll Now</Button>
-      </div>
-      <section className="flex flex-col justify-center">
-        <div className="m-20 p-9 px-5 py-8 rounded-2xl border-2 border-gray-700">
-          <ProfileForm />
-        </div>
-        <div className="flex flex-row items-center gap-8 mb-4">
-          <CalendarDemo />
-          <CardWithForm />
-        </div>
-      </section>
-      <div>
-        <TableDemo />
-      </div>
-      <div>
-        <CommandDemo />
-      </div>
-    </main>
+    <div className="container relative">
+      <PageHeader>
+        <Announcement />
+        <PageHeaderHeading>Build your component library</PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can copy and paste into your
+          apps. Accessible. Customizable. Open Source.
+        </PageHeaderDescription>
+        <PageActions>
+          <Button> Get Started</Button>
+          <Button variant={"secondary"}>GitHub</Button>
+        </PageActions>
+      </PageHeader>
+      <Nav className="[&>a:first-child]:text-primary" />
+    </div>
   );
 }
+
+
+
+
+// export default function Home() {
+//   return (
+    // <main className="p-24">
+    //   <Nav />
+    //   <section className="py-8 flex flex-col items-center text-center gap-4">
+    //     <h1 className="text-6xl font-bold">Shadcn is Amazing</h1>
+    //     <p className="text-2xl text-muted-foreground">
+    //       Hand-picked themes that you can copy and paste into your apps <br />
+    //     </p>
+    //   </section>
+    //   <div className="flex items-center justify-center gap-2">
+    //     <Button variant={"secondary"}>Learn More</Button>
+    //     <Button variant={"default"}>Enroll Now</Button>
+    //   </div>
+    //   <section className="flex flex-col justify-center">
+    //     <div className="m-20 p-9 px-5 py-8 rounded-2xl border-2 border-gray-700">
+    //       <ProfileForm />
+    //     </div>
+    //     <div className="flex flex-row items-center gap-8 mb-4">
+    //       <CalendarDemo />
+    //       <CardWithForm />
+    //     </div>
+    //   </section>
+    //   <div>
+    //     <TableDemo />
+    //   </div>
+    //   <div>
+    //     <CommandDemo />
+    //   </div>
+    // </main>
+//   );
+// }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,35 +1,145 @@
 "use client";
 
-import Image from "next/image";
-import { ModeToggle } from "./ui/toggle-mode";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
 
-export default function Nav() {
+import { cn } from "@/lib/utils";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
+
+
+const examples = [
+  {
+    name: "Mail",
+    href: "/examples/mail",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/mail",
+  },
+  {
+    name: "Dashboard",
+    href: "/examples/dashboard",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/dashboard",
+  },
+  {
+    name: "Cards",
+    href: "/examples/cards",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/cards",
+  },
+  {
+    name: "Tasks",
+    href: "/examples/tasks",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/tasks",
+  },
+  {
+    name: "Playground",
+    href: "/examples/playground",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/playground",
+  },
+  {
+    name: "Forms",
+    href: "/examples/forms",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/forms",
+  },
+  {
+    name: "Music",
+    href: "/examples/music",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/music",
+  },
+  {
+    name: "Authentication",
+    href: "/examples/authentication",
+    code: "https://github.com/shadcn/ui/tree/main/apps/www/app/examples/authentication",
+  },
+];
+
+interface NavProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Nav({ className, ...props }: NavProps) {
+  const pathname = usePathname();
+
   return (
-    <header>
-      <nav>
-        <ul className="flex items-center justify-between">
-          <li>
-            <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+    <div className="relative">
+      <ScrollArea className="max-w-[600px] lg:max-w-none">
+        <div className={cn("mb-4 flex items-center", className)} {...props}>
+          {examples.map((example, index) => (
+            <Link
+              href={example.href}
+              key={example.href}
+              className={cn(
+                "flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors hover:text-primary",
+                pathname?.startsWith(example.href) ||
+                  (index === 0 && pathname === "/")
+                  ? "bg-muted font-medium text-primary"
+                  : "text-muted-foreground"
+              )}
             >
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className="dark:invert"
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </li>
-          <li>
-            <ModeToggle />
-          </li>
-        </ul>
-      </nav>
-    </header>
+              {example.name}
+            </Link>
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" className="invisible" />
+      </ScrollArea>
+    </div>
   );
 }
+
+interface ExampleCodeLinkProps {
+  pathname: string | null;
+}
+
+export function ExampleCodeLink({ pathname }: ExampleCodeLinkProps) {
+  const example = examples.find((example) =>
+    pathname?.startsWith(example.href)
+  );
+
+  if (!example?.code) {
+    return null;
+  }
+
+  return (
+    <Link
+      href={example?.code}
+      target="_blank"
+      rel="nofollow"
+      className="absolute right-0 top-0 hidden items-center rounded-[0.5rem] text-sm font-medium md:flex"
+    >
+      View code
+      <ArrowRightIcon className="ml-1 h-4 w-4" />
+    </Link>
+  );
+}
+
+// "use client";
+
+// import Image from "next/image";
+// import { ModeToggle } from "./ui/toggle-mode";
+
+// export default function Nav() {
+//   return (
+//     <header>
+//       <nav>
+//         <ul className="flex items-center justify-between">
+//           <li>
+//             <a
+//               className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+//               href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//             >
+//               <Image
+//                 src="/vercel.svg"
+//                 alt="Vercel Logo"
+//                 className="dark:invert"
+//                 width={100}
+//                 height={24}
+//                 priority
+//               />
+//             </a>
+//           </li>
+//           <li>
+//             <ModeToggle />
+//           </li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
